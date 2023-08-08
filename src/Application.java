409f9590684838e777275bd4edb469f1d2f5fcac
g2smdm <138638273+g2smdm@users.noreply.github.com>
2023-08-08 23:22:44 +0530

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
 Class Application {
+  int multiply(int a,int b)
+    {
+    return a*b;
+  }
   Public static void main(String args[])
     {
+    System.out.println("multiply:+multiply(2,3));
     System.out.println("Welcome to Java");
   }
 }
